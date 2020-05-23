0324ed861eb9f8960a529d498808c91fbddd092f
Pascal <pacolos@web.de>
2020-05-23 19:26:01 +0200

    Better ItemBar

--- Assets/Resources/ItemBar/ItemBarScript.cs
+++ Assets/Resources/ItemBar/ItemBarScript.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemBarScript : MonoBehaviour
 {
     
-    public void CreateItemBar(Inventory inv)
+    /*public void CreateItemBar(Inventory inv)
     {
         float x = 33f;
         float y = -32f;
@@ -17,6 +18,26 @@ public class ItemBarScript : MonoBehaviour
                 Debug.Log("Object could not be loaded");
             }
             var newItem = Instantiate(obj);
+            newItem.GetComponent<RectTransform>().SetParent(transform);
+            newItem.GetComponent<RectTransform>().anchoredPosition =  new Vector3(x, y, 0);
+            x += 47f;
+        }
+    }*/
+    
+    public void CreateItemBar(Inventory inv)
+    {
+        float x = 33f;
+        float y = -32f;
+        foreach(var item in inv.Items)
+        {
+            var obj = Resources.Load<GameObject>("ItemBar/Prefabs/ItemIcon");
+            if (obj == null)
+            {
+                Debug.Log("Object could not be loaded");
+            }
+            
+            var newItem = Instantiate(obj);
+            newItem.GetComponent<RawImage>().texture = Resources.Load<Texture>("Items/Tiles/" + item.ItemName);
             newItem.GetComponent<RectTransform>().SetParent(transform);
             newItem.GetComponent<RectTransform>().anchoredPosition =  new Vector3(x, y, 0);
             x += 47f;
