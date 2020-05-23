cdbc702a8982f6cf2346f6adc4b8a7ea1ee96f10
Pascal <pacolos@web.de>
2020-05-23 19:55:49 +0200

    PickUp fixes

--- Assets/Resources/ItemBar/ItemBarScript.cs
+++ Assets/Resources/ItemBar/ItemBarScript.cs
@@ -5,25 +5,6 @@ using UnityEngine.UI;
 
 public class ItemBarScript : MonoBehaviour
 {
-    
-    /*public void CreateItemBar(Inventory inv)
-    {
-        float x = 33f;
-        float y = -32f;
-        foreach(var item in inv.Items)
-        {
-            var obj = Resources.Load<GameObject>("ItemBar/Prefabs/"+item.ItemName);
-            if (obj == null)
-            {
-                Debug.Log("Object could not be loaded");
-            }
-            var newItem = Instantiate(obj);
-            newItem.GetComponent<RectTransform>().SetParent(transform);
-            newItem.GetComponent<RectTransform>().anchoredPosition =  new Vector3(x, y, 0);
-            x += 47f;
-        }
-    }*/
-    
     public void CreateItemBar(Inventory inv)
     {
         float x = 33f;
--- Assets/Resources/PickUps/Scripts/PickUpScript.cs
+++ Assets/Resources/PickUps/Scripts/PickUpScript.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class PickUpScript : MonoBehaviour
 {
+    private GameItem _item;
     // Start is called before the first frame update
-    public void CreatePickUp(string item)
+    public void SetPickUpItem(string itemName)
     {
+        _item = GameItem.ConstructItem(itemName);
         SpriteRenderer sr = gameObject.GetComponentInChildren<SpriteRenderer>();
-        sr.sprite = Resources.Load<Sprite>("Items/Tiles/" + item);
+        sr.sprite = Resources.Load<Sprite>("Items/Tiles/" + _item.ItemName);
+    }
+
+    public void OnInfoZoneEnter()
+    {
+        //TODO
+    }
+
+    public void OnPickUp()
+    {
+        //TODO
     }
 }
